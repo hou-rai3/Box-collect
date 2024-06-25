1cdcf7533c45e0db3b1f211c9e4ab7e4a79149e8
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-06-25 17:02:36 +0900

    リミットで開始だったのを即動作に変更(リミットなかった)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ InterruptIn button_stert(PC_9); // 起動スイッチ
 CAN can(PA_11, PA_12, (int)1e6);
 CANMessage msg;
 uint8_t DATA[8] = {0};
-int DJI_ID = 0x200;
+int DJI_ID = 0x1FF;
 int16_t speed = 0;
 
 bool flag = false;
@@ -35,32 +35,32 @@ int main()
         auto now_1 = HighResClock::now();
         bool sw_stop = button_stop.read();
         bool sw_stert = button_stert.read();
+        printf("Stert\n");
+        speed = 8000;
 
-        if (sw_stert == 0)
-        {
-            printf("Stert\n");
-            speed = 8000;
+        int16_t signed_speed = static_cast<int16_t>(-speed);
+        DATA[0] = (signed_speed >> 8) & 0xFF; // 上位バイト
+        DATA[1] = signed_speed & 0xFF;        // 下位バイト
+        signed_speed = static_cast<int16_t>(speed);
+        DATA[2] = (signed_speed >> 8) & 0xFF; // 上位バイト
+        DATA[3] = signed_speed & 0xFF;        // 下位バイト
 
-            int16_t signed_speed = static_cast<int16_t>(-speed);
-            DATA[0] = (signed_speed >> 8) & 0xFF; // 上位バイト
-            DATA[1] = signed_speed & 0xFF;        // 下位バイト
-            signed_speed = static_cast<int16_t>(speed);
-            DATA[2] = (signed_speed >> 8) & 0xFF; // 上位バイト
-            DATA[3] = signed_speed & 0xFF;        // 下位バイト
+        pre = now;
+        can.reset(); // CANコントローラをリセット
+        // if (sw_stert == 0)
+        // {
 
-            pre = now;
-            can.reset(); // CANコントローラをリセット
-        }
-        else
-        { // 何もしない
-        }
-        if (sw_stop = 0)
-        {
-            stop_motor(0);
-        }
-        else
-        { // 何もしない
-        }
+        // }
+        // else
+        // { // 何もしない
+        // }
+        // if (sw_stop == 0)
+        // {
+        //     stop_motor(0);
+        // }
+        // else
+        // { // 何もしない
+        // }
 
         if (now_1 - pre_1 > 30ms)
         {

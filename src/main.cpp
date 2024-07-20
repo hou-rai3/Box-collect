c460c2ac384359a49fe7ff7b89a9f658a5e26f75
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-07-20 14:34:26 +0900

    関数増やした

--- src/main.cpp
+++ src/main.cpp
@@ -14,74 +14,63 @@ bool flag = false;
 
 void stop_motor(int zero)
 {
-    printf("STOP\n");
-    for (int i = 0; i < 4; i += 2)
-    {
-        DATA[i] = (zero >> 8) & 0xFF;
-        DATA[i + 1] = zero & 0xFF;
-    }
+  printf("STOP\n");
+  for (int i = 0; i < 4; i += 2)
+  {
+    DATA[i] = (zero >> 8) & 0xFF;
+    DATA[i + 1] = zero & 0xFF;
+  }
 }
+void motor_move(int speed)
+{
+  int16_t signed_speed = static_cast<int16_t>(speed);
+  DATA[2] = (signed_speed >> 8) & 0xFF;
+  DATA[3] = signed_speed & 0xFF;
 
+  signed_speed = static_cast<int16_t>(-speed);
+  DATA[4] = (signed_speed >> 8) & 0xFF;
+  DATA[5] = signed_speed & 0xFF;
+
+  signed_speed = static_cast<int16_t>(speed);
+  DATA_1[6] = (signed_speed >> 8) & 0xFF;
+  DATA_1[7] = signed_speed & 0xFF;
+}
 int main()
 {
-    button_stop.mode(PullUp);
-    button_stert.mode(PullUp);
-    BufferedSerial pc(USBTX, USBRX, 115200);
-    auto pre = HighResClock::now();
+  button_stop.mode(PullUp);
+  button_stert.mode(PullUp);
+  BufferedSerial pc(USBTX, USBRX, 115200);
+  auto pre = HighResClock::now();
 
-    while (1)
+  while (1)
+  {
+    auto now = HighResClock::now();
+    bool sw_stop = button_stop.read();
+
+    if (!sw_stop)
     {
-        auto now = HighResClock::now();
-
-        bool sw_stop = button_stop.read();
-        if (!sw_stop)
-        {
-            printf("Stert\n");
-            speed = 16000;
-
-            int16_t signed_speed = static_cast<int16_t>(-speed);
-            DATA[2] = (signed_speed >> 8) & 0xFF;
-            DATA[3] = signed_speed & 0xFF;
-
-            signed_speed = static_cast<int16_t>(-speed);
-            DATA[4] = (signed_speed >> 8) & 0xFF;
-            DATA[5] = signed_speed & 0xFF;
-
-            signed_speed = static_cast<int16_t>(speed);
-            DATA_1[6] = (signed_speed >> 8) & 0xFF;
-            DATA_1[7] = signed_speed & 0xFF;
-        }
-        else
-        {
-            speed = 0;
-            int16_t signed_speed = static_cast<int16_t>(speed);
-            DATA[2] = (signed_speed >> 8) & 0xFF;
-            DATA[3] = signed_speed & 0xFF;
-
-            signed_speed = static_cast<int16_t>(-speed);
-            DATA[4] = (signed_speed >> 8) & 0xFF;
-            DATA[5] = signed_speed & 0xFF;
-
-            signed_speed = static_cast<int16_t>(speed);
-            DATA_1[6] = (signed_speed >> 8) & 0xFF;
-            DATA_1[7] = signed_speed & 0xFF;
-        }
-
-        if (now - pre > 30ms)
-        {
-            CANMessage msg(DJI_ID, DATA, 8);
-            CANMessage msg1(0x200, DATA_1, 8);
-            if (can.write(msg) && can.write(msg1))
-            {
-            }
-            else
-            {
-                printf("Can't send Message\n");
-                printf("CAN Bus Error Status: %d\n", can.rderror());
-                printf("CAN Bus Write Error Count: %d\n", can.tderror());
-                can.reset();
-            }
-            pre = now;
-        }
+      motor_move(10000);
     }
+    else
+    {
+      motor_move(0);
+    }
+
+    if (now - pre > 30ms)
+    {
+      CANMessage msg(DJI_ID, DATA, 8);
+      CANMessage msg1(0x200, DATA_1, 8);
+      if (can.write(msg) && can.write(msg1))
+      {
+      }
+      else
+      {
+        printf("Can't send Message\n");
+        printf("CAN Bus Error Status: %d\n", can.rderror());
+        printf("CAN Bus Write Error Count: %d\n", can.tderror());
+        can.reset();
+      }
+      pre = now;
+    }
+  }
 }
